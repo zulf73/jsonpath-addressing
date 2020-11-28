24ed0e863df72ad2d8347637a45fa5570a00c598
Zulf Ahmed <zulf@pop-os.localdomain>
2020-11-28 02:39:37 -0600

    Stub code for ui and need set/get based on JSPath

--- server/main.js
+++ server/main.js
@@ -1,5 +1,9 @@
 import { Meteor } from 'meteor/meteor';
+import fs from 'fs';
+import {Session} from 'meteor/session';
 
 Meteor.startup(() => {
-  // code to run on server at startup
+    var raw_data = fs.readFileSync('IPIP120.json');
+    var survey_json = JSON.parse( raw_data );
+    Session.set('doc', survey_json );
 });
