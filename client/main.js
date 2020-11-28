b4302ea4d1647d03f35bf273cbfbf3b722baae27
Zulf Ahmed <zulf@pop-os.localdomain>
2020-11-28 16:25:54 -0600

    Does not work yet

--- client/main.js
+++ client/main.js
@@ -1,4 +1,6 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 import { Session } from 'meteor/session';
+import {raw_data} from '/imports/api/IPIP120.js';
+Session.set('doc', raw_data );
 import '/imports/ui/body';
--- server/main.js
+++ server/main.js
@@ -1,9 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 import fs from 'fs';
-import {Session} from 'meteor/session';
 
 Meteor.startup(() => {
-    var raw_data = fs.readFileSync('IPIP120.json');
-    var survey_json = JSON.parse( raw_data );
-    Session.set('doc', survey_json );
+
 });
